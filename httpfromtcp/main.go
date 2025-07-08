310ff84852005eeeed773c0b123de93a3aba2eca
Daniel Eberle <daniel.j.eberle@protonmail.com>
2025-07-08 00:22:39 -0500

    HTTP Streams L4 - Newlines

--- httpfromtcp/main.go
+++ httpfromtcp/main.go
@@ -1,31 +1,56 @@
 package main
 
 import (
+    "errors"
     "fmt"
+    "log"
     "io"
     "os"
+    "strings"
 )
 
+const inputFilePath = "messages.txt"
+
 func main() {
-    f,err := os.Open("./messages.txt")
+    f,err := os.Open(inputFilePath)
     if err != nil {
-        panic(err)
+        log.Fatalf("Could not open %s: %s\n", inputFilePath, err)
     }
 
     defer f.Close()
 
-    buf := make([]byte, 8)
+    fmt.Printf("Reading data from %s\n", inputFilePath)
+    fmt.Println("=====================================")
+
+    currentLine := ""
 
     for {
-        chunk,err := f.Read(buf)
-        if err != nil && err != io.EOF {
-            panic(err)
-        }
+        buf := make([]byte, 8, 8)
 
-        if err == io.EOF {
+        chunk,err := f.Read(buf)
+        if err != nil {
+
+            if errors.Is(err, io.EOF) {
+                break
+            }
+
+            fmt.Printf("error: %s\n", err.Error())
             break
         }
 
-        fmt.Printf("read: %s\n", buf[:chunk])
+        parts := strings.Split(string(buf[:chunk]), "\n")
+
+        currentLine += parts[0]
+
+        if len(parts) == 1 {
+            continue
+        }
+
+        fmt.Printf("read: %s\n", currentLine)
+        for i := 1; i < len(parts)-1; i++ {
+            fmt.Printf("read: %s\n", parts[i])
+        }
+
+        currentLine = parts[len(parts)-1]
     }
 }
